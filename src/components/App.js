ab11a3efdc88b4955439d894c52dcb41b309f577
yuanguo813 <yuanguo813@gmail.com>
2020-08-25 14:46:45 -0400

    initial configuration of webpack with React

---
+++ src/components/App.js
@@ -0,0 +1,12 @@
+import React from 'react';
+import '../styling/App.css';
+
+function App() {
+  return (
+    <div className="app">
+      <h1>Covid-19 Tracking App</h1>
+    </div>
+  );
+}
+
+export default App;
